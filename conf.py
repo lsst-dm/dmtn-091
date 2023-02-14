3d689abd9f9988585e4b9cde88fb9ea024ee4896
Jeff Carlin <jcarlin@lsst.org>
2023-02-14 10:44:15 -0800

    Fix config error

--- conf.py
+++ conf.py
@@ -5,12 +5,3 @@ https://documenteer.lsst.io/technotes/configuration.html
 """
 
 from documenteer.conf.technote import *  # noqa: F401, F403
-
-# Ingest settings from metadata.yaml and use documenteer's configure_technote()
-# to build a Sphinx configuration that is injected into this script's global
-# namespace.
-metadata_path = os.path.join(os.path.dirname(__file__), 'metadata.yaml')
-with open(metadata_path, 'r') as f:
-    confs = configure_technote(f)
-g = globals()
-g.update(confs)
